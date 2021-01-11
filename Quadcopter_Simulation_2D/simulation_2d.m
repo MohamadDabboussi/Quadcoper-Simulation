104e0bc0cd3c8b3d7a3699edfe69354cf08ee606
Mohamad Dabboussi <dabboussi99@gmail.com>
2021-01-11 19:19:18 +0200

    Add 2D simulation

--- Quadcopter_Simulation_2D/simulation_2d.m
+++ Quadcopter_Simulation_2D/simulation_2d.m
@@ -1,9 +1,6 @@
-function [t_out, z_out] = height_control(trajhandle, controlhandle)
-
-addpath('utils');
+function [t_out, s_out] = simulation_2d(controlhandle, trajhandle)
 
 video = false;
-video_filename = 'height_control.avi';
 
 params = sys_params;
 
@@ -13,7 +10,7 @@ real_time = true;
 %% **************************** FIGURES *****************************
 disp('Initializing figures...')
 if video
-  video_writer = VideoWriter(video_filename, 'Uncompressed AVI');
+  video_writer = VideoWriter('test_control_2d.avi', 'Uncompressed AVI');
   open(video_writer);
 end
 h_fig = figure;
@@ -23,43 +20,50 @@ xpos = ceil((screensize(3)-sz(1))/2); % center the figure on the screen horizont
 ypos = ceil((screensize(4)-sz(2))/2); % center the figure on the screen vertically
 set(h_fig, 'Position', [xpos ypos sz])
 
-h_3d = subplot(1,2,1);
+h_3d = subplot(3,3,[1,2,4,5,7,8]);
 axis equal
 grid on
-view(0,0);
-xlabel('x [m]'); ylabel('y [m]'); zlabel('z [m]');
-
-h_2d = subplot(1,2,2);
-plot_2d = plot(h_2d, 0, 0);
-grid on;
-xlabel('t [s]'); ylabel('z [m]');
+view(90,0);
+ylabel('y [m]'); zlabel('z [m]');
 
 quadcolors = lines(1);
 
 set(gcf,'Renderer','OpenGL')
 
 %% *********************** INITIAL CONDITIONS ***********************
-max_iter  = 320;       % max iteration
-starttime = 0;         % start of simulation in seconds
-tstep     = 0.005;      % this determines the time step at which the solution is given
-cstep     = 0.05;      % image capture time interval
-nstep     = cstep/tstep;
-time      = starttime; % current time
+t_total  = 7;             % Total simulated time
+tstep    = 0.01;          % this determines the time step at which the solution is given
+cstep    = 0.05;          % image capture time interval
+max_iter = t_total/cstep; % max iteration
+nstep    = cstep/tstep;
+time     = 0; % current time
+err = []; % runtime errors
 % Get start and stop position
-des_start = trajhandle(0);
-des_stop  = trajhandle(inf);
-stop_pos  = des_stop(1);
-x0        = des_start;
+des_start = trajhandle(0,[]);
+des_stop  = trajhandle(inf,[]);
+
+% Get boundary
+d_state = nan(max_iter,2);
+for iter = 1:max_iter
+    dd = trajhandle(cstep*iter,[]);
+    d_state(iter,:) = dd.pos(1:2)';
+end
+y_lim = [min(d_state(:,1)) - 0.1, max(d_state(:,1)) + 0.1];
+z_lim = [min(d_state(:,2)) - 0.1, max(d_state(:,2)) + 0.1];
+if(4*(z_lim(2) - z_lim(1)) < y_lim(2) - y_lim(1))
+    z_lim(1) = z_lim(1) - (y_lim(2) - y_lim(1))/8;
+    z_lim(2) = z_lim(2) + (y_lim(2) - y_lim(1))/8;
+end
+stop_pos = des_stop.pos;
+x0        = [des_start.pos; 0; des_start.vel; 0];
 xtraj     = nan(max_iter*nstep, length(x0));
 ttraj     = nan(max_iter*nstep, 1);
 
 x         = x0;        % state
 
-pos_tol   = 0.01;
-vel_tol   = 0.01;
-
-kv=1;
-kp=2;
+pos_tol = 0.01;
+vel_tol = 0.02;
+ang_tol = 0.05;
 
 %% ************************* RUN SIMULATION *************************
 disp('Simulation Running....')
@@ -71,22 +75,17 @@ for iter = 1:max_iter
   tic;
   % Initialize quad plot
   if iter == 1
-    subplot(1,2,1);
+    subplot(3,3,[1,2,4,5,7,8]);
     quad_state = simStateToQuadState(x0);
     QP = QuadPlot(1, quad_state, params.arm_length, 0.05, quadcolors(1,:), max_iter, h_3d);
+    ylim(y_lim); zlim(z_lim);
     quad_state = simStateToQuadState(x);
     QP.UpdateQuadPlot(quad_state, time);
     h_title = title(h_3d, sprintf('iteration: %d, time: %4.2f', iter, time));
   end
 
   % Run simulation
-  %[xsave,tsave]=wsysRK4V(@(t,s) sys_eom(t, s, controlhandle, trajhandle, params,kv,kp),x,time,time+cstep,10);
-  %tsave
-  %xsave
-  %[tsave, xsave] = ode45(@(t,s) sys_eom(t, s, controlhandle, trajhandle, params,kv,kp), timeint, x);
-  ode23(
-  tsave
-  xsave
+  [tsave, xsave] = ode45(@(t,s) sys_eom(t, s, controlhandle, trajhandle, params), timeint, x);
   x = xsave(end, :)';
 
   % Save to traj
@@ -94,22 +93,32 @@ for iter = 1:max_iter
   ttraj((iter-1)*nstep+1:iter*nstep) = tsave(1:end-1);
 
   % Update quad plot
-  subplot(1,2,1)
   quad_state = simStateToQuadState(x);
   QP.UpdateQuadPlot(quad_state, time + cstep);
+  subplot(3,3,[1,2,4,5,7,8]);
+  ylim(y_lim); zlim(z_lim);
   set(h_title, 'String', sprintf('iteration: %d, time: %4.2f', iter, time + cstep))
   time = time + cstep; % Update simulation time
-
-  set(plot_2d, 'XData', ttraj(1:iter*nstep), 'YData', xtraj(1:iter*nstep,1));
   if video
     writeVideo(video_writer, getframe(h_fig));
   end
+    subplot(3,3,3)
+    plot(ttraj(1:iter*nstep), xtraj(1:iter*nstep,1));
+    xlabel('t [s]'); ylabel('y [m]');
+    grid on;
+    subplot(3,3,6)
+    plot(ttraj(1:iter*nstep), xtraj(1:iter*nstep,2));
+    xlabel('t [s]'); ylabel('z [m]');
+    grid on;
+    subplot(3,3,9)
+    plot(ttraj(1:iter*nstep), 180/pi*xtraj(1:iter*nstep,3));
+    grid on;
+    xlabel('t [s]'); ylabel('\phi [deg]');
 
   t = toc;
   % Check to make sure ode45 is not timing out
   if(t > cstep*50)
-    err = 'Ode solver took too long for a step. Maybe the controller is unstable.';
-    disp(err);
+    err = 'Ode45 Unstable';
     break;
   end
 
@@ -118,14 +127,13 @@ for iter = 1:max_iter
     pause(cstep - t);
   end
 
+  % Check termination criteria
+  if norm(x(1:2) - stop_pos) < pos_tol && norm(x(4:5)) < vel_tol && abs(x(3)) < ang_tol
+    err = [];
+    break
+  end
+  err = 'Did not reach goal';
 end
-% Check termination criteria
-if norm(stop_pos - x(1)) < pos_tol && norm(x(2)) < vel_tol
-  err = [];
-else
-  err = 'Did not converge';
-end
-
 disp('Simulation done');
 if video
   close(video_writer);
@@ -133,8 +141,10 @@ end
 
 if ~isempty(err)
   disp(['Error: ', err]);
+else
+  disp(['Final time: ', num2str(time), ' sec']);
 end
 t_out = ttraj(1:iter*nstep);
-z_out = xtraj(1:iter*nstep,1);
+s_out = xtraj(1:iter*nstep,:);
 
 end
