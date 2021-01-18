ed1e72d1f5305554d1036040a9eb7732777dfba9
Mohamad Dabboussi <dabboussi99@gmail.com>
2021-01-18 14:50:05 +0200

    add 3D keyboard control

--- Quadcopter_Simulation-3D/KeyPress.m
+++ Quadcopter_Simulation-3D/KeyPress.m
@@ -2,19 +2,19 @@
 function KeyPress(~,key)
     switch key.Key
         case 'w'
-            evalin('base','Data =[0 0 0.5]');
+            evalin('base','Data =Data+[0 0 0.5]');
         case 's'
-             evalin('base','Data = [0 0 -0.5]');
+             evalin('base','Data =Data+ [0 0 -0.5]');
        case 'a'
-            evalin('base','Data = [0 0.5 0]');
+            evalin('base','Data =Data+ [0 0.5 0]');
         case 'd'
-             evalin('base','Data = [0 -0.5 0]');
+             evalin('base','Data =Data+ [0 -0.5 0]');
        case 'c'
-            evalin('base','Data = [0.5 0 0]');
+            evalin('base','Data =Data+ [0.5 0 0]');
         case 'z'
-             evalin('base','Data = [-0.5 0 0]');
+             evalin('base','Data =Data+ [-0.5 0 0]');
         case 'q'
             evalin('base','finish = 1');
     end
-    pause(0.02);
+    pause(0.05);
 end
--- Quadcopter_Simulation-3D/quadEOM.m
+++ Quadcopter_Simulation-3D/quadEOM.m
@@ -1,4 +1,4 @@
-function sdot = quadEOM(t, s, qn, controlhandle, trajhandle, params,Data)
+function sdot = quadEOM(t, s, qn, controlhandle, trajhandle, params,Data,speed,opt)
 % QUADEOM Wrapper function for solving quadrotor equation of motion
 % 	quadEOM takes in time, state vector, controller, trajectory generator
 % 	and parameters and output the derivative of the state vector, the
@@ -17,20 +17,40 @@ function sdot = quadEOM(t, s, qn, controlhandle, trajhandle, params,Data)
 %
 % NOTE: You should not modify this function
 % See Also: quadEOM_readonly, crazyflie
+ if ~exist('speed','var')
+     % third parameter does not exist, so default it to something
+      speed = 1;
+ end
+  if ~exist('opt','var')
+     % third parameter does not exist, so default it to something
+      speed = 7;
+ end
 
 % convert state to quad stuct for control
 qd{qn} = stateToQd(s);
 
 % Get desired_state
+
+if (isequal(trajhandle,@diamond))
 desired_state = trajhandle(t, qn);
-
 % The desired_state is set in the trajectory generator
-
 qd{qn}.pos_des      = qd{qn}.pos;
 qd{qn}.vel_des      = Data';
 qd{qn}.acc_des      = desired_state.acc;
 qd{qn}.yaw_des      = 0;
 qd{qn}.yawdot_des   = 0;
+end
+
+if (isequal(trajhandle,@trajectory_generator))
+ desired_state = trajhandle(t, qn,Data,speed,opt);
+
+% The desired_state is set in the trajectory generator
+qd{qn}.pos_des      = desired_state.pos;
+qd{qn}.vel_des      = desired_state.vel;
+qd{qn}.acc_des      = desired_state.acc;
+qd{qn}.yaw_des      = desired_state.yaw;
+qd{qn}.yawdot_des   = desired_state.yawdot;
+end
 
 % get control outputs
 [F, M, trpy, drpy] = controlhandle(qd, t, qn, params);
